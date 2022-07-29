7819698774dcd34e4951840f15a4fb38afefcfd1
Aviral Nimekar <aviralnimekar12@gmail.com>
2022-07-29 14:50:06 +0530

    Refactor logger factory with sls4j

--- src/main/java/com/test/rabbitmqproducer/consumer/RabbitMQConsumer.java
+++ src/main/java/com/test/rabbitmqproducer/consumer/RabbitMQConsumer.java
@@ -1,23 +1,21 @@
 package com.test.rabbitmqproducer.consumer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class RabbitMQConsumer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
 
-    @RabbitListener(queues = {"${rabbitmq.queue.name}"},
-            errorHandler = "")
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
     public void consume(String message) {
 
         if ("exception".equals(message)) {
             throw new RuntimeException("Custom Exception");
         }
 
-        LOGGER.info(String.format("Received message -> %s", message));
+        log.info(String.format("Received message -> %s", message));
     }
 }
