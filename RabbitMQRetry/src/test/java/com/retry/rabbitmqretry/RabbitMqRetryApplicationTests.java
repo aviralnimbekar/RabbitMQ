ac1aae81eeef927ef027ed56c2711a1ffee14bd7
Aviral Nimekar <aviralnimekar12@gmail.com>
2022-07-29 16:24:52 +0530

    Add retry module

---
+++ RabbitMQRetry/src/test/java/com/retry/rabbitmqretry/RabbitMqRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retry.rabbitmqretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqRetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
